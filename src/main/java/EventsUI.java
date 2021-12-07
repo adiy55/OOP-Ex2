b812a10d99d5283f08f4eaed3a354cc632173ba6
adiy55 <adiyafe55@gmail.com>
2021-12-08 01:10:50 +0200

    Add UI classes

---
+++ src/main/java/EventsUI.java
@@ -0,0 +1,165 @@
+import api.DirectedWeightedGraph;
+import api.DirectedWeightedGraphAlgorithms;
+import api.EdgeData;
+import api.NodeData;
+import javafx.event.ActionEvent;
+import javafx.geometry.Point2D;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+// todo: action to insert new path
+//          action to show algorithms (i.e. mark shortest path)
+public class EventsUI {
+    public static DirectedWeightedGraphAlgorithms algo;
+
+    public static ArrayList<Line> initGraphUI() {
+        DirectedWeightedGraph graph = algo.getGraph();
+        ArrayList<Line> lines = new ArrayList<>();
+        Iterator<EdgeData> edge_iter = graph.edgeIter();
+        while (edge_iter.hasNext()) {
+            EdgeData e = edge_iter.next();
+            NodeData src = graph.getNode(e.getSrc());
+            NodeData dest = graph.getNode(e.getDest());
+            lines.add(new Line(src.getLocation().x() * 10000000, src.getLocation().y() * 10000000, dest.getLocation().x(), dest.getLocation().y()));
+        }
+        return lines;
+    }
+
+    public static Stage getInputNode() {
+
+        Stage stage = new Stage();
+        stage.setTitle("Add Node");
+
+        Label id = new Label("Insert ID:");
+        Label loc_x = new Label("Insert X coordinate:");
+        Label loc_y = new Label("Insert Y coordinate:");
+        Label loc_z = new Label("Insert Z coordinate:");
+        Label l = new Label();
+
+        Button button = new Button("Submit");
+        TextField text1 = new TextField();
+        TextField text2 = new TextField();
+        TextField text3 = new TextField();
+        TextField text4 = new TextField();
+        button.setOnAction((ActionEvent event) ->
+                {
+                    text1.getText();
+                    text2.getText();
+                    text3.getText();
+                    text4.getText();
+                    GeoLoc gl = new GeoLoc(Double.parseDouble(text2.getText()), Double.parseDouble(text3.getText()), Double.parseDouble(text4.getText()));
+                    Node n = new Node(Integer.parseInt(text1.getText()), gl);
+                    algo.getGraph().addNode(n);
+                    l.setText(String.format("Node %d was added", n.getKey()));
+
+                }
+        );
+
+        VBox layout = new VBox(5);
+        layout.getChildren().addAll(id, text1, loc_x, text2, loc_y, text3, loc_z, text4, button, l);
+
+        Scene scene = new Scene(layout, 300, 280);
+        stage.setScene(scene);
+
+        return stage;
+    }
+
+    public static Stage getInputEdge() {
+
+        Stage stage = new Stage();
+        stage.setTitle("Add Edge");
+
+        Label src = new Label("Insert source node:");
+        Label dest = new Label("Insert destination node:");
+        Label w = new Label("Insert edge weight:");
+        Label l = new Label();
+
+        Button button = new Button("Submit");
+        TextField text1 = new TextField();
+        TextField text2 = new TextField();
+        TextField text3 = new TextField();
+        button.setOnAction((ActionEvent event) ->
+                {
+                    text1.getText();
+                    text2.getText();
+                    text3.getText();
+                    algo.getGraph().connect(Integer.parseInt(text1.getText()), Integer.parseInt(text2.getText()), Double.parseDouble(text3.getText()));
+                    l.setText(String.format("Edge (%s,%s) was added", text1.getText(), text2.getText()));
+                }
+        );
+
+        VBox layout = new VBox(5);
+        layout.getChildren().addAll(src, text1, dest, text2, w, text3, button, l);
+
+        Scene scene = new Scene(layout, 300, 250);
+        stage.setScene(scene);
+
+        return stage;
+    }
+
+    public static Stage deleteNode() {
+
+        Stage stage = new Stage();
+        stage.setTitle("Remove Node");
+
+        Label id = new Label("Insert Node ID:");
+        Label l = new Label();
+
+        Button button = new Button("Submit");
+        TextField text1 = new TextField();
+        button.setOnAction((ActionEvent event) ->
+                {
+                    text1.getText();
+                    algo.getGraph().removeNode(Integer.parseInt(text1.getText()));
+                    l.setText(String.format("Node %s was removed", text1.getText()));
+                }
+        );
+
+        VBox layout = new VBox(5);
+        layout.getChildren().addAll(id, text1, button, l);
+
+        Scene scene = new Scene(layout, 300, 250);
+        stage.setScene(scene);
+
+        return stage;
+    }
+
+    public static Stage deleteEdge() {
+
+        Stage stage = new Stage();
+        stage.setTitle("Remove Edge");
+
+        Label src = new Label("Insert Edge src:");
+        Label dest = new Label("Insert Edge dest:");
+
+        Label l = new Label();
+
+        Button button = new Button("Submit");
+        TextField text1 = new TextField();
+        TextField text2 = new TextField();
+        button.setOnAction((ActionEvent event) ->
+                {
+                    text1.getText();
+                    text2.getText();
+                    algo.getGraph().removeEdge(Integer.parseInt(text1.getText()), Integer.parseInt(text2.getText()));
+                    l.setText(String.format("Edge (%s,%s) was removed", text1.getText(), text2.getText()));
+                }
+        );
+
+        VBox layout = new VBox(5);
+        layout.getChildren().addAll(src, text1, dest, text2, button, l);
+
+        Scene scene = new Scene(layout, 300, 250);
+        stage.setScene(scene);
+
+        return stage;
+    }
+}
---
+++ src/main/java/GraphUI.java
@@ -0,0 +1,80 @@
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+// todo: fix margins, update graph after adding a node
+public class GraphUI extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        initGUI(stage);
+
+    }
+
+    private void initGUI(Stage stage) {
+        stage.setTitle("Directed Weighted Graph UI");
+        Group root = new Group();
+        Scene scene = new Scene(new VBox(), 400, 350);
+
+        MenuBar menu_bar = new MenuBar();
+        VBox vbox = new VBox();
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(10);
+        vbox.setPadding(new Insets(0, 10, 0, 10));
+
+        Menu menu_file = new Menu("File");
+        MenuItem exit = new MenuItem("Exit");
+        exit.setOnAction((ActionEvent event) -> Platform.exit());
+        MenuItem save = new MenuItem("Save");
+        MenuItem load = new MenuItem("Load");
+        menu_file.getItems().addAll(save, load, exit);
+
+        Menu menu_edit = new Menu("Edit");
+        Menu add = new Menu("Add");
+        MenuItem add_node = new MenuItem("Add Node");
+        EventHandler<ActionEvent> node_event = actionEvent -> EventsUI.getInputNode().show();
+        add_node.setOnAction(node_event);
+        MenuItem add_edge = new MenuItem("Add Edge");
+        EventHandler<ActionEvent> edge_event = actionEvent -> EventsUI.getInputEdge().show();
+        add_edge.setOnAction(edge_event);
+        Menu delete = new Menu("Delete");
+        MenuItem del_node = new MenuItem("Delete Node");
+        EventHandler<ActionEvent> dnode_event = actionEvent -> EventsUI.deleteNode().show();
+        del_node.setOnAction(dnode_event);
+        MenuItem del_edge = new MenuItem("Delete Edge");
+        EventHandler<ActionEvent> dedge_event = actionEvent -> EventsUI.deleteEdge().show();
+        del_edge.setOnAction(dedge_event);
+        delete.getItems().addAll(del_node, del_edge);
+        add.getItems().addAll(add_node, add_edge);
+        menu_edit.getItems().addAll(add, delete);
+
+        Button b = new Button("Algorithm");
+        String[] choices = {"isConnected", "shortestPathDist", "shortestPath", "center", "tsp"};
+        ChoiceDialog<Object> dialog = new ChoiceDialog<>(new Separator(), choices);
+        dialog.setTitle("Run Algorithm");
+        EventHandler<ActionEvent> event = actionEvent -> dialog.show();
+        b.setOnAction(event);
+
+        root.getChildren().addAll(EventsUI.initGraphUI());
+
+        menu_bar.getMenus().addAll(menu_file, menu_edit);
+        ToolBar toolbar = new ToolBar();
+        toolbar.getItems().addAll(menu_bar, b);
+        ((VBox) scene.getRoot()).getChildren().addAll(toolbar, new HBox(root), vbox);
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+
+
+}
